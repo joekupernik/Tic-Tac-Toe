d0f5aff000fcca05e7946a51b0eb7776aa995e01
Joe Kupernik <joekupernik@gmail.com>
2021-12-07 15:18:18 -0800

    render update

--- src/index.js
+++ src/index.js
@@ -94,7 +94,9 @@ function Square(props) {
       return (
         <div className="game">
           <div className="game-board">
-            <Board />
+            <Board 
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)} />
           </div>
           <div className="game-info">
             <div>{/* status */}</div>
